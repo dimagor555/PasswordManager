db3e08146898b4744a95a9de52cdaff936c71eb8
poliant0log <esed.pol@mail.ru>
2020-12-30 18:18:23 +0300

    add: window shows

--- javafx/src/main/java/ru/dimagor555/javafxapp/Main.java
+++ javafx/src/main/java/ru/dimagor555/javafxapp/Main.java
@@ -1,4 +1,22 @@
 package ru.dimagor555.javafxapp;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root = new Pane();
+        Scene scene = new Scene(root);
+
+        primaryStage = new Stage();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Password Manager");
+        primaryStage.setOnCloseRequest(event -> System.exit(0));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
 }
