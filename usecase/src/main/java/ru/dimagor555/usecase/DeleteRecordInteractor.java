7ea9982cb2ec6cb00c799d85310cfa792440a44a
dimagor555 <dimagor555@mail.ru>
2021-01-17 01:54:44 +0300

    add: DeleteRecord usecase

---
+++ usecase/src/main/java/ru/dimagor555/usecase/DeleteRecordInteractor.java
@@ -0,0 +1,16 @@
+package ru.dimagor555.usecase;
+
+import ru.dimagor555.domain.entity.Record;
+import ru.dimagor555.domain.port.RecordRepository;
+
+public class DeleteRecordInteractor extends RecordInteractor implements DeleteRecord {
+    public DeleteRecordInteractor(RecordRepository recordRepository) {
+        super(recordRepository);
+    }
+
+    @Override
+    public void execute(Record record, Callback callback) {
+        recordRepository.delete(record);
+        callback.onRecordDeleted();
+    }
+}
