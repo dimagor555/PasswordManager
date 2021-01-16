ed538b2aa7c7b12f699e8d62888f9d3b37d5a820
dimagor555 <dimagor555@mail.ru>
2021-01-16 19:58:27 +0300

    fix: RecordCreationModel uses preview feature - record

--- usecase/src/main/java/ru/dimagor555/usecase/RecordCreationModel.java
+++ usecase/src/main/java/ru/dimagor555/usecase/RecordCreationModel.java
@@ -1,4 +1,25 @@
 package ru.dimagor555.usecase;
 
-public record RecordCreationModel(String site, String login, String password) {
+public final class RecordCreationModel {
+    private final String site;
+    private final String login;
+    private final String password;
+
+    public RecordCreationModel(String site, String login, String password) {
+        this.site = site;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String site() {
+        return site;
+    }
+
+    public String login() {
+        return login;
+    }
+
+    public String password() {
+        return password;
+    }
 }
