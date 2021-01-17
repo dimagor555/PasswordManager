f4b5c0e2dca34a4c60f31cf5ffb362eec4fe5b41
dimagor555 <dimagor555@mail.ru>
2021-01-18 02:48:42 +0300

    add: MainPresenter full implementation

--- presentation/src/main/java/ru/dimagor555/presentation/MainPresenter.java
+++ presentation/src/main/java/ru/dimagor555/presentation/MainPresenter.java
@@ -1,28 +1,56 @@
 package ru.dimagor555.presentation;
 
+import ru.dimagor555.domain.entity.Record;
+import ru.dimagor555.usecase.GetAllRecords;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class MainPresenter {
+    private final GetAllRecords getAllRecords;
     private final Navigator navigator;
     private View view;
 
-    public MainPresenter(Navigator navigator) {
+    public MainPresenter(GetAllRecords getAllRecords, Navigator navigator) {
+        this.getAllRecords = getAllRecords;
         this.navigator = navigator;
     }
 
-    public void setView(View view) { this.view = view; }
-
-    public void updateSelectedRecord() {
-        navigator.openUpdateWindow();
+    public void setView(View view) {
+        this.view = view;
     }
 
-    public void createSelectedRecord() {
+    public void update() {
+        getAllRecords.execute(allRecords -> {
+            List<Record> records = new ArrayList<>(allRecords);
+            records.sort(Comparator.comparing(Record::getSite)
+                    .thenComparing(Record::getLogin));
+            view.renderRecords(records);
+        });
+    }
+
+    public void updateSelectedRecord() {
+        Record selected = view.getSelected();
+        if (selected != null) {
+            navigator.openUpdateWindow(selected);
+        }
+    }
+
+    public void createRecord() {
         navigator.openCreateWindow();
     }
 
     public void deleteSelectedRecord() {
-        navigator.openDeleteWindow();
+        Record selected = view.getSelected();
+        if (selected != null) {
+            navigator.openDeleteWindow(selected);
+        }
     }
 
     public interface View {
+        Record getSelected();
 
+        void renderRecords(List<Record> records);
     }
 }
