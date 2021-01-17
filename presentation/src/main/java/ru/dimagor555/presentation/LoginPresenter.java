89bc1302d9a66366b83394652da3f90da46bbf3a
dimagor555 <dimagor555@mail.ru>
2021-01-18 02:45:28 +0300

    add: LoginPresenter full implementation

--- presentation/src/main/java/ru/dimagor555/presentation/LoginPresenter.java
+++ presentation/src/main/java/ru/dimagor555/presentation/LoginPresenter.java
@@ -1,21 +1,36 @@
 package ru.dimagor555.presentation;
 
+import ru.dimagor555.usecase.Login;
+
 public class LoginPresenter {
+    private final Login login;
     private final Navigator navigator;
     private View view;
 
-    public LoginPresenter(Navigator navigator) {
+    public LoginPresenter(Login login, Navigator navigator) {
+        this.login = login;
         this.navigator = navigator;
     }
 
     public void login() {
         String password = view.getPassword();
-        if (password.equals("test")) {
-            navigator.openMainWindow();
-            view.hidePasswordError();
-        } else {
-            view.showPasswordError();
-        }
+        login.execute(password, new Login.Callback() {
+            @Override
+            public void onSuccessfulLogin() {
+                navigator.openMainWindow();
+                navigator.closeLoginWindow();
+            }
+
+            @Override
+            public void onIncorrectPassword() {
+                view.showPasswordError();
+            }
+
+            @Override
+            public void onMasterPasswordNotFound() {
+                navigator.showMasterPasswordNotFoundDialog();
+            }
+        });
     }
 
     public void setView(View view) { this.view = view; }
